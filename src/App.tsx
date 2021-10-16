446f72a29a054c05442affa73fcf523826dfe901
chell2 <rei1259@gmail.com>
2021-10-16 23:46:22 +0900

    form

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
 	return (
 		<div className="App">
 			<div className="hero min-h-auto text-center bg-base-100">
-				<div className="card shadow-lg mt-4">
+				<div className="card shadow-lg mt-2">
 					<div className="card-body">
 						<h1 className="card-title">被災状況の報告</h1>
 						<form
@@ -47,8 +47,7 @@ const App: React.FC = () => {
 								<label>被害にあった施設・機械</label>
 								<input
 									name="damage_target"
-									type="text"
-									className="input input-success input-bordered"
+									className="textarea h-20 max-h-48 textarea-bordered textarea-success"
 									placeholder="ビニールハウス、排水ポンプ、管理機など"
 								/>
 							</div>
@@ -56,7 +55,7 @@ const App: React.FC = () => {
 								<label>現地の状況</label>
 								<textarea
 									name="damege_status"
-									className="textarea h-24 max-h-48 textarea-bordered textarea-success"
+									className="textarea h-20 max-h-48 textarea-bordered textarea-success"
 									placeholder="詳細"
 								></textarea>
 							</div>
